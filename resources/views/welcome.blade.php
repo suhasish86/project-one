7deac72ee6921add67ec7195273d2e2d362a469c
suhasish86 <suhasish86@gmail.com>
2020-03-26 16:21:27 +0530

    New commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,6 +84,10 @@
                     Laravel Test Project
                 </div>
 
+                <div class="content">
+                    <div class="row"></div>
+                </div>
+
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
